f4e82eb1bf36848e128a283cda94af95d1a5a079
steimo <vladyslav.kolomiets@protonmail.com>
2023-01-08 23:18:14 +0200

    add routing and controller class

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'rulers/array'
+require 'rulers/routing'
 require_relative "rulers/version"
 
 module Rulers
@@ -7,9 +8,35 @@ module Rulers
 
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, { 'Content-Type' => 'text/html' },
-       ["Hello from Ruby on Rulers! #{[1,2,3,4,5].sum}"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+                { 'Content-Type' => 'text/html' }, []]
+      elsif env['PATH_INFO'] == '/'
+        return [200,
+                { 'Content-Type' => 'text/html' }, [File.read('./public/index.html')]]
+        # return [302, { 'Location' => "/quotes/a_quote" }, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+        [200, { 'Content-Type' => 'text/html' },
+         [text]]
+      rescue
+        return [500,
+                { 'Content-Type' => 'text/html' }, ['Oh no, something wrong just happened!']]
+      end
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
